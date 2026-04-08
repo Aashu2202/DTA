9a9a70b10a23c59f8a0725f8ced2434acb3d0874
AdarshShrivastava29 <adarshshrivastava7987@gmail.com>
2026-04-08 12:01:07 +0530

    comment add

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,2 +1,3 @@
 // export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 export const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://dtableanalytics.com';
+// live url: https://dtableanalytics.com
